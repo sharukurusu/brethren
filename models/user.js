0b3ad21580faab1718b4998232ba6ce5b72931d4
M.Zarli <16181950+sharukurusu@users.noreply.github.com>
2018-12-04 19:47:58 -0500

    updated users-info handlebars

--- models/user.js
+++ models/user.js
@@ -1,10 +1,11 @@
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     name: DataTypes.STRING,
-    email: DataTypes.STRING,
+    email: { type: DataTypes.STRING, validate: { isEmail: true } },
     password: DataTypes.STRING,
     securityQuestion: DataTypes.STRING,
     bio: DataTypes.TEXT,
+    favorites: DataTypes.TEXT,
     picture: DataTypes.BLOB("long")
   });
   return User;
