f97512d6fd84592c420b4a40c8bd93cdd539b8f8
Donte Ojeda <donte.ojeda@gmail.com>
2018-12-06 21:47:32 -0500

    spotify api set up

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,8 +6,11 @@ var $message = $("#message"),
   $handle = $("#handle"),
   $btn = $("#send"),
   $output = $("#output"),
-  $feedback = $("#feedback");
-
+  $feedback = $("#feedback"),
+  $trackSearch = $("#track-search"),
+  $artistSearch = $("#artist-search"),
+  $trackBtn = $("#trackSend"),
+  $artistBtn = $("#artistSend");
 //Emit events
 
 $btn.on("click", function(event) {
@@ -23,6 +26,24 @@ $message.on("keypress", function() {
   socket.emit("typing", $handle.val());
 });
 
+$trackBtn.on("click", function(event) {
+  if ($trackSearch.val() === "") {
+    return false;
+  }
+  console.log($trackSearch.val());
+  event.preventDefault();
+  socket.emit("trackSearch", {
+    search: $trackSearch.val()
+  });
+});
+
+$artistBtn.on("click", function(event) {
+  event.preventDefault();
+  socket.emit("artistSearch", {
+    search: $artistSearch.val()
+  });
+});
+
 //Listen for events
 
 socket.on("chat", function(data) {
@@ -36,4 +57,8 @@ socket.on("typing", function(data) {
       "<p id='" + data + "'><em>" + data + " is typing a message...</em></p>"
     );
   }
+});
+
+socket.on("trackSearch", function(data) {
+  console.log(data);
 });
