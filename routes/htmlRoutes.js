04c93b7dc6bc7e6a8f55ffe69919574a2a14cc6e
Donte Ojeda <donte.ojeda@gmail.com>
2018-12-08 11:05:38 -0500

    DA BIG PUSH

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,32 +1,44 @@
-var db = require("../models");
+// Requiring path to so we can use relative routes to our HTML files
+var path = require("path");
+
+// Requiring our custom middleware for checking if a user is logged in
+var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
-  // Load index page
+
   app.get("/", function(req, res) {
-    db.User.findAll({}).then(function(dbusers) {
-      res.render("index", {
-        msg: "Welcome!",
-        users: dbusers
-      });
-    });
+    // If the user already has an account send them to the members page
+    // console.log(req.user)
+    // if (req.user) {
+    //   res.redirect("/members");
+    // }
+    // res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.render("signup")
   });
 
-  // Load user page and pass in an user by id
-  app.get("/user/:id", function(req, res) {
-    db.User.findOne({ where: { id: req.params.id } }).then(function(dbuser) {
-      res.render("user", {
-        user: dbuser
-      });
-    });
+  app.get("/login", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    // res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.render("login")
   });
 
-  //Render chatroom page
-  app.get("/chat", function(req, res) {
-    res.render("chat");
+  app.get("/chat", isAuthenticated, function(req, res) {
+    // res.sendFile(path.join(__dirname, "../public/chat.html"));
+    res.render("chat")
   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/members", isAuthenticated, function(req, res) {
+    res.render("profile")
+    // res.sendFile(path.join(__dirname, "../public/members.html"));
   });
+
+//   app.get("*", function(req, res) {
+//     res.render("404");
+//   });
+
 };
