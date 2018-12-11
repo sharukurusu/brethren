268ae338e59e72ddb311332ffb244394a4852aa9
M.Zarli <16181950+sharukurusu@users.noreply.github.com>
2018-12-10 19:20:38 -0500

    image upload form

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var server = require("http").createServer(app);
 var io = require("socket.io").listen(server);
 // For image uploads
 var multer = require("multer");
-var upload = multer({ dest: "./uploads" });
+var path = require("path")
 
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
@@ -80,6 +80,61 @@ io.sockets.on("connection", function(socket) {
     io.sockets.emit("connected users", connectedChatUsers);
   });
 });
+
+// // Image upload
+// //  Set The Storage Engine
+// const storage = multer.diskStorage({
+//   destination: './public/uploads/',
+//   filename: function(req, file, cb){
+//     cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+//   }
+// });
+
+// // Init Upload
+// const upload = multer({
+//   storage: storage,
+//   limits:{fileSize: 1000000},
+//   fileFilter: function(req, file, cb){
+//     checkFileType(file, cb);
+//   }
+// }).single('myImage');
+
+// // Check File Type
+// function checkFileType(file, cb){
+//   // Allowed ext
+//   const filetypes = /jpeg|jpg|png|gif/;
+//   // Check ext
+//   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+//   // Check mime
+//   const mimetype = filetypes.test(file.mimetype);
+
+//   if(mimetype && extname){
+//     return cb(null,true);
+//   } else {
+//     cb('Error: Images Only!');
+//   }
+// }
+// // Image upload route
+// app.post('/upload', (req, res) => {
+//     upload(req, res, (err) => {
+//       if(err){
+//         res.render('index', {
+//           msg: err
+//         });
+//       } else {
+//         if(req.file == undefined){
+//           res.render('index', {
+//             msg: 'Error: No File Selected!'
+//           });
+//         } else {
+//           res.render('index', {
+//             msg: 'File Uploaded!',
+//             file: `uploads/${req.file.filename}`
+//           });
+//         }
+//       }
+//     });
+//   });
 
 // Requiring our routes
 require("./routes/htmlRoutes.js")(app);
