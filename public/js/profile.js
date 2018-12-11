eee6de61f221cff09ab00b1852e3878717277f73
Donte Ojeda <donte.ojeda@gmail.com>
2018-12-10 19:24:59 -0500

    minor changes

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
 
-    var $editBioText = $("#edit-bio-textbox")
-    var $editBioSubmit = $("#edit-bio-submit")
+    
+    var $editBtn = $("#edit-profile")
     var $username = $("#user-title").attr("data-username")
     var $bio = $("#bio")
 
-    $editBioSubmit.on("click", function(event){
-        event.preventDefault()
-        var newBio = $editBioText.val().trim()
-        $editBioText.val("")
+    // $editBioSubmit.on("click", function(event){
+    //     event.preventDefault()
+    //     var newBio = $editBioText.val().trim()
+    //     $editBioText.val("")
 
-        $.ajax({
-            method: "PUT",
-            url: "/api/bio",
-            data: {username: $username, bio: newBio}
-        }).then(function(){
-            $.get("/api/users/" + $username)
-            .then(function(data){
-                $bio.text("Bio: " + data.bio)
-            })
-        })
-    })
+    //     $.ajax({
+    //         method: "PUT",
+    //         url: "/api/bio",
+    //         data: {username: $username, bio: newBio}
+    //     }).then(function(){
+    //         $.get("/api/users/" + $username)
+    //         .then(function(data){
+    //             $bio.text("Bio: " + data.bio)
+    //         })
+    //     })
+    // })
 });
